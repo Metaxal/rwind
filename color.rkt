7c33746a18a0f4bd35b00efa8a99cdeeeffdfef7
Metaxal <laurent.orseau@gmail.com>
2012-11-18 12:19:24 +0100

    adding file

---
+++ color.rkt
@@ -0,0 +1,39 @@
+#lang racket/base
+
+(require rwind/base
+         rwind/doc-string
+         x11-racket/x11
+         racket/dict
+         )
+
+#|
+- (in French) http://pficheux.free.fr/articles/lmf/xlib-images/
+- List of existing colors:
+  $ tail /etc/X11/rgb.txt -n +2 | sed -e 's/^\s*//' -e 's/\s\s*/ /g'| cut -d' ' -f4,5
+  (colors are case insensitive)
+  - see also color-database<%> in Racket's help, but not all colors may be supported by default 
+|#
+
+(define* black-pixel #f)
+(define* white-pixel #f)
+
+;; Cache the colors in a dictionary.
+;; Maybe it would be a better idea to use Racket's own database... 
+;; (but not sure how to deal with that)
+(define color-database (make-hash))
+
+(define* (find-named-color color-str [default-color black-pixel])
+  (dict-ref! color-database color-str
+             (λ()
+               (define disp (current-display))
+               (define screen (DefaultScreen disp))
+               (AllocNamedColor disp screen color-str default-color))))
+
+(define* (init-colors)
+  (define disp (current-display))
+  (define screen (DefaultScreen disp))
+
+  (set! black-pixel (BlackPixel disp screen))
+  (set! white-pixel (WhitePixel disp screen))  
+  )
+  
