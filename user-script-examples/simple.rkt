1948f0e12f89655c761431a7a79edc7505cbb683
Metaxal <laurent.orseau@gmail.com>
2012-11-20 14:39:32 +0100

    updating simple.rkt

--- user-script-examples/simple.rkt
+++ user-script-examples/simple.rkt
@@ -10,24 +10,36 @@
          racket/function)
 
 ;;; Some key/mouse bindings
-;; Mod1Mask is Alt/Meta
+;;; Mod1Mask is Alt/Meta
+;;; Mod4Mask is the Super key (Windows key on PCs)
 
+;; Open xterm
 (bind-key global-keymap "t" '(Mod1Mask ControlMask)
-          (thunk* (rwind-system "xterm")))
-(bind-key global-keymap "c" '(Mod1Mask ControlMask)
-          (thunk* (rwind-system "xclock -digital -update 1")))
-(bind-key global-keymap "F2" '(Mod1Mask)
-          (thunk* (rwind-system "gmrun")))
-(bind-key global-keymap "F12" '(Mod1Mask)
-          (thunk* (rwind-system "xterm -g 80x24+400+0 -T 'RWind Client' -e 'racket -e \"(require rwind/client)\"'")))
-(bind-key global-keymap "F10" '(Mod1Mask)
-          (thunk* (open-user-config-file)))
+          (L* (rwind-system "xterm")))
 
+;; Open xclock
+(bind-key global-keymap "c" '(Mod1Mask ControlMask)
+          (L* (rwind-system "xclock -digital -update 1")))
+
+;; Open gmrun (requires it to be installed)
+(bind-key global-keymap "F2" '(Mod1Mask)
+          (L* (rwind-system "gmrun")))
+
+;; Opens the client of rwind for console interaction
+(bind-key global-keymap "F12" '(Mod1Mask)
+          (L* (rwind-system "xterm -g 80x24+400+0 -T 'RWind Client' -e 'racket -e \"(require rwind/client)\"'")))
+
+;; Open the config file for editing, with "open" on mac or "xdg-open" on Linux
+(bind-key global-keymap "F10" '(Mod1Mask)
+          (L* (open-user-config-file)))
+
+;; Switch to the first workspace
 (bind-key global-keymap "F1" '(Mod4Mask)
           (L* (activate-workspace 0)))
+
+;; Switch to the second workspace
 (bind-key global-keymap "F2" '(Mod4Mask)
           (L* (activate-workspace 1)))
-
 
 ;; Left-click to focus and raise window
 (bind-button window-keymap 1 'ButtonPress '()
