2567a188ccf7417b0c23c3d6cf3f2213ba827f59
Metaxal <laurent.orseau@gmail.com>
2013-05-27 15:27:44 +0200

    moved click-to-focus out of config file.

--- user-script-examples/simple.rkt
+++ user-script-examples/simple.rkt
@@ -19,7 +19,7 @@
  ; Open xclock
  "M-C-c" (L* (rwind-system "xclock -digital -update 1"))
  ; Open gmrun (requires it to be installed)
- "M-F2"  (L* (displayln "Running gm-run")(rwind-system "gmrun"))
+ "M-F2"  (L* (rwind-system "gmrun"))
  ; Opens the client of rwind for console interaction
  "M-F12" (L* (rwind-system "xterm -g 80x24+400+0 -T 'RWind Client' -e 'racket -e \"(require rwind/client)\"'"))
  ; Open the config file for editing, with "open" on mac or "xdg-open" or "mimeopen" on Linux
@@ -39,7 +39,7 @@
    global-keymap
    ; Switch to the i-th workspace with Super-F1, Super-F2, etc.
    (format "Super-F~a" (add1 i)) (L* (activate-workspace i))
-   ; Move window to workspace and activate
+   ; Move window to workspace and switch to workspace
    (format "S-Super-F~a" (add1 i)) (L* (move-window-to-workspace/activate (input-focus) i))
    ))
 
@@ -51,20 +51,7 @@
  "M-Move3" (motion-resize-window)
  )
 
-(add-binding
- window-keymap
- ;; Left-click to focus and raise window.
- ;; We need to use the grab sync mode in order to be able to replay the event to the window
- ;; after we have processsed it, using allow-events.
- ;; (see http://tronche.com/gui/x/xlib/input/XGrabPointer.html)
- ;; (see metacity/src/core/display.c around line 1728)
- "Press1" (λ(ev)
-            (define w (keymap-event-window ev))
-            (raise-window w)
-            (set-input-focus w)
-            (allow-events 'ReplayPointer)
-            )
- #:grab-mode 'GrabModeSync)
+(bind-click-to-focus "Press1")
 
 (add-bindings 
  root-keymap 
