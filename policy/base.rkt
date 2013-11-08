cd7188ceb8695a00f0d9e256129129f1066a6b0d
Laurent Orseau <laurent.orseau@gmail.com>
2013-11-08 15:54:19 +0100

    add comments

--- policy/base.rkt
+++ policy/base.rkt
@@ -30,37 +30,51 @@ and it must necessarily be  implemented to have a working WM.
 
 (define* policy%
   (class object%
-    
+
+    ;; Called when a key is pressed.
+    ;; Keybindings have already been called.
     (define/public (on-keypress keyboard-ev)
       (void))
     
+    ;; Called when a mouse button is pressed.
+    ;; Mouse button bindings have already been called.
     (define/public (on-mouse-button mouse-ev)
       (void))
     
+    ;; Called when the mouse moves.
+    ;; Mouse and keybindings have already been called.
     (define/public (on-motion-notify mouse-ev)
       (void))
     
+    ;; Called when a window has requested to be mapped
     (define/public (on-map-request window new?)
       (void))
     
+    ;; Called after a window has been unmapped
     (define/public (on-unmap-notify window)
       (void))
     
+    ;; Called after a window has been destroyed
     (define/public (on-destroy-notify window)
       (void))
     
     ;; Called on an inner call to create-simple-window,
-    ;; i.e., not on an X event.
+    ;; i.e., not on an X event
     (define/public (on-create-window window)
       (void))
     
+    ;; Called after a window has been created
     (define/public (on-create-notify window)
       (void))
     
+    ;; Called after a window has been requested for configuration.
+    ;; It is up to the policy to choose whether this request should be honored.
     (define/public (on-configure-request window value-mask
                                          x y width height border-width above stack-mode)
       (void))
     
+    ;; Called after the true root window has been configured,
+    ;; for example after a resolution change, a newly plugged screen, etc.
     (define/public (on-configure-notify-true-root)
       (void))
     
@@ -68,22 +82,27 @@ and it must necessarily be  implemented to have a working WM.
     (define/public (activate-window window)
       (void))
     
-    ;; Gives the keyboard focus to the next window in the list of windows.
+    ;; Gives the keyboard focus to the next window in the list of windows
     (define/public (activate-next-window)
       (void))
 
+    ;; Called after a window has been added to the workspace wk
     (define/public (on-add-window-to-workspace window wk)
       (void))
     
+    ;; Called after a window has been removed from the workspace wk
     (define/public (on-remove-window-from-workspace window wk)
       (void))
     
+    ;; Called after workspaces have been initialized
     (define/public (on-init-workspaces)
       (void))
     
+    ;; Called after a workspace has been activated
     (define/public (on-activate-workspace wk)
       (void))
     
+    ;; Called after the workspace mode (single, multi) has been changed
     (define/public (on-change-workspace-mode mode)
       (void))
     
