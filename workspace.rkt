f9ccd866ef88a5110220cb6ab276530c14d3123d
Laurent Orseau <laurent.orseau@gmail.com>
2013-11-09 19:39:47 +0100

    Can circulate windows in the workspace

--- workspace.rkt
+++ workspace.rkt
@@ -231,18 +231,22 @@ This is mainly meant to be used to restore windows to their proper workspaces."
   (workspace? . -> . any)
   "Moves the first window to the bottom, and raises all the other windows.
   This has no effect on the X stacking order."
-  (define wl (workspace-windows wk))
-  (unless (empty? wk)
-    (set-workspace-windows! wk (append (rest wl) (list (first wl))))))
+  (define-values (shown hidden) (partition window-viewable? (workspace-windows wk)))
+  (unless (or (empty? shown) 
+              (empty? (rest shown)))
+    (set-workspace-windows! 
+     wk
+     (append (rest shown) (list (first shown)) hidden))))
 
 (define*/contract (workspace-circulate-windows-down wk)
   (workspace? . -> . any)
   "Moves the last window to the top, and lowers all the other windows.
   This has no effect on the X stacking order."
-  (define wl (workspace-windows wk))
-  (unless (empty? wk)
-    (define-values (a b) (split-at-right wl 1))
-    (set-workspace-windows! wk (append b a))))
+  (define-values (shown hidden) (partition window-viewable? (workspace-windows wk)))
+  (unless (or (empty? shown) 
+              (empty? (rest shown)))
+    (define-values (a b) (split-at-right shown 1))
+    (set-workspace-windows! wk (append b a hidden))))
 
 (define*/contract (workspace-focus-in w [wk (find-window-workspace w)])
   ([window?] [workspace?] . ->* . any)
