865e872cef7edf2a317ae063784d1c971404b06c
Metaxal <laurent.orseau@gmail.com>
2018-01-20 09:59:50 +0000

    fix testing

--- info.rkt
+++ info.rkt
@@ -1,3 +1,8 @@
 #lang info
 
 (define deps '("x11" "base" "rackunit-lib" "slideshow-lib" "readline-lib" "gui-lib"))
+
+(define test-omit-paths
+  '("client.rkt"
+    "user-files"
+    "launcher.rkt"))
